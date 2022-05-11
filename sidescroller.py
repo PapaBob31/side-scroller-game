eeca6fac8d48d6ef06541d67241e70d526a36b51
damola <egbebitimi121@gmail.com>
2022-05-11 13:28:04 -0800

    added spikes and good background image

--- sidescroller.py
+++ sidescroller.py
@@ -1,17 +1,20 @@
 import pygame, copy, random
 pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("bg_music.ogg")
 pygame.display.set_caption("Run Box run!")
-win = pygame.display.set_mode((800, 450))
-bg_img = pygame.image.load('background.jpg')
+win = pygame.display.set_mode((800, 420))
+bg_img = pygame.image.load('bg_img.jpg').convert()
+text = pygame.font.SysFont("Helvetica", 18)
 clock = pygame.time.Clock()
 
 red = (255, 0, 0)
-green = (108, 184, 132)# (126, 191, 147)
+green = (106, 231, 127)
 
 class Player:
 	def __init__(self):
 		self.x = 200
-		self.y = 360
+		self.y = 330
 		self.color = red
 		self.width = 40
 		self.height = 40
@@ -26,7 +29,7 @@ class Player:
 
 	def reset(self):
 		self.x = 200
-		self.y = 360
+		self.y = 330
 		self.vel = 10
 		self.acc_up = False
 		self.acc_down = False
@@ -36,14 +39,13 @@ class Player:
 
 class Rectangle:
 	"""This is an Obstacle class"""
-	def __init__(self, x, y, width, height):
+	def __init__(self, x, y, width, height, is_floating=False):
 		self.x = x
 		self.y = y
 		self.color = green
 		self.width = width
 		self.height = height
-		self.lower = False
-		self.same_height = False
+		self.is_floating = is_floating
 
 	def display(self):
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
@@ -51,32 +53,36 @@ class Rectangle:
 
 class Triangle:
 	"""This is an Obstacle class"""
-	def __init__(self, x, base):
+	def __init__(self, x, y, width):
 		self.x = x
-		self.base = base
-		self.y = self.base-40
-		self.width = 40
+		self.y = y
+		self.width = width
+		self.height = width
+		self.apex_x = x + (self.width//2)
+		self.apex_y = y - self.height
+		self.right_base_corner = x+width
 		self.color = green
 
 	def display(self):
-		pygame.draw.polygon(win, self.color, ((self.x, self.base),  (self.x+(self.width//2), self.y), (self.x+self.width, self.base)))
-
+		pygame.draw.polygon(win, self.color, ((self.x, self.y), (self.apex_x, self.apex_y), (self.right_base_corner, self.y)))
 
 class Platform():
 	def __init__(self):
 		self.x = 0
-		self.y = 400
+		self.y = 370
 		self.color = green
 		self.width = 800
 		self.height = 50
 		self.obstacles_vel = 10
-		self.obstacles_list = [Rectangle(800, 360, 80, 40), Rectangle(800, 360, 120, 40), Rectangle(800, 320, 80, 80),
-							   Rectangle(800, 320, 80, 80), Rectangle(800, 280, 80, 40), Rectangle(800, 280, 120, 120), 
-							   Rectangle(800, 280, 80, 120), Rectangle(800, 240, 80, 40), Rectangle(800, 320, 120, 80)]
-		self.obstacles_onscreen = [Rectangle(800, 360, 80, 40)]
+		self.obstacles_list = [Rectangle(800, 330, 80, 40), Rectangle(800, 330, 120, 40), Rectangle(800, 290, 80, 80),
+							   Rectangle(800, 290, 80, 80), Rectangle(800, 250, 80, 40, True), Rectangle(800, 250, 120, 120), 
+							   Rectangle(800, 250, 80, 120), Rectangle(800, 210, 80, 40, True), Rectangle(800, 290, 120, 80)]
+		self.obstacles_onscreen = [Triangle(640, 370, 20), Triangle(660, 370, 20),
+								   Triangle(680, 370, 20), Rectangle(700, 330, 80, 40)]
 		self.obstacle_under_box = None
 		self.next_obstacle = None
 		self.game_over = False
+		self.score = 0
 
 	def display(self):
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
@@ -84,36 +90,69 @@ class Platform():
 box = Player()
 platform = Platform()
 
+def createSpikes(no_of_spikes, last_obst):
+	spike_x = last_obst.x + last_obst.width
+	for no in range(0, no_of_spikes):
+		spike = Triangle(spike_x, 370, 20)
+		spike = copy.copy(spike)
+		platform.obstacles_onscreen.append(spike)
+		spike_x = platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width
+
 def create_next_obstacle():
 	obstacle = random.choice(platform.obstacles_list)
 	platform.next_obstacle = copy.copy(obstacle)
 	if (obstacle.y - 40) == platform.obstacles_onscreen[-1].y:
-		platform.next_obstacle.lower = True
+		if obstacle.is_floating:
+			createSpikes(2 + obstacle.width//20, platform.obstacles_onscreen[-1])
+		else:
+			createSpikes(2, platform.obstacles_onscreen[-1])
+
 	elif obstacle.y == (platform.obstacles_onscreen[-1].y - 40):
-		platform.next_obstacle.lower = False
+		if obstacle.is_floating:
+			createSpikes(5 + obstacle.width//20, platform.obstacles_onscreen[-1])
+		else:
+			createSpikes(5, platform.obstacles_onscreen[-1])
+
 	elif obstacle.y == platform.obstacles_onscreen[-1].y:
-		platform.next_obstacle.same_height = True
+		if obstacle.is_floating:
+			createSpikes(7 + obstacle.width//20, platform.obstacles_onscreen[-1])
+		else:
+			createSpikes(7, platform.obstacles_onscreen[-1])
+
 	else:
 		create_next_obstacle()
 
 def reset():
-	platform.obstacles_onscreen = [Rectangle(800, 360, 80, 40)]
+	platform.obstacles_onscreen =[Triangle(640, 370, 20), Triangle(660, 370, 20),
+								   Triangle(680, 370, 20), Rectangle(700, 330, 80, 40)]
 	platform.next_obstacle = None
 	platform.obstacle_under_box = None
 	platform.game_over = False
 	box.reset()
 	platform.obstacles_vel = 10
 
+
+def createObstaclesAndSpikes():
+	create_next_obstacle()
+	if platform.next_obstacle.is_floating:
+		index = platform.next_obstacle.width//20
+		platform.next_obstacle.x = platform.obstacles_onscreen[-index].x
+	else:
+		platform.next_obstacle.x = platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width
+	platform.obstacles_onscreen.append(platform.next_obstacle)
+	platform.next_obstacle = None
+
 run = True
 i = 0
+pygame.mixer.music.play(-1, 0, 0)
 
 while run:
-	pygame.time.delay(20)
+	pygame.time.delay(22)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP and not box.acc_down:
+			if event.key == pygame.K_UP and not box.acc_down and not box.acc_up:
 				if not platform.game_over:
 					box.init_pos = box.y
 					box.acc_up = True
@@ -123,6 +162,7 @@ while run:
 	win.fill((0, 0, 0))
 	win.blit(bg_img, (i, 0))
 	win.blit(bg_img, (800+i, 0))
+	win.blit(text.render("SCORE: " + str(platform.score), True, (0, 255, 0)), (700, 10))
 	box.display()
 	platform.display()
 
@@ -138,7 +178,7 @@ while run:
 		box.y += box.vel
 		if box.y == box.init_pos + 20:
 			box.vel = 10
-		if box.y == 360:
+		if box.y == 330:
 			box.init_pos = box.y
 			box.acc_up = False
 			box.acc_down = False
@@ -147,45 +187,47 @@ while run:
 		if i == -800:
 			i = 0
 		i -= 10
-		if not platform.next_obstacle:
-			create_next_obstacle()
-
-		if not platform.next_obstacle.same_height:
-			if platform.next_obstacle.lower:
-				if platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width <= 760:
-					platform.obstacles_onscreen.append(platform.next_obstacle)
-					platform.next_obstacle = None
-			elif not platform.next_obstacle.lower:
-				if platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width <= 700:
-					platform.obstacles_onscreen.append(platform.next_obstacle)
-					platform.next_obstacle = None
-		else:
-			if platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width <= 660:
-				platform.obstacles_onscreen.append(platform.next_obstacle)
-				platform.next_obstacle = None
-
+		if platform.obstacles_onscreen[-1].x < 800:
+			createObstaclesAndSpikes()
+			
 	for obst in platform.obstacles_onscreen[:]:
 		obst.display()
-		obst.x -= platform.obstacles_vel
+		if type(obst) == Triangle:
+			obst.x -= platform.obstacles_vel
+			obst.apex_x -= platform.obstacles_vel
+			obst.right_base_corner -= platform.obstacles_vel
+		else:
+			obst.x -= platform.obstacles_vel
 
 	for obst in platform.obstacles_onscreen[:]:
 		if obst.x + obst.width <= 0:
+			if type(obst) == Rectangle:
+				platform.score += 1
 			platform.obstacles_onscreen.remove(obst)
 
 	for obst in platform.obstacles_onscreen[:]:
-		if box.acc_down and type(obst) == Rectangle:
-			if box.y + 40 == obst.y:
-				if box.x in range(obst.x, obst.x + obst.width):
-					box.acc_down = False
-					platform.obstacle_under_box = obst
-				if obst.x in range(box.x, box.x + box.width):
-					box.acc_down = False
-					platform.obstacle_under_box = obst
-		if box.y in range(obst.y, obst.y + obst.height) or obst.y in range(box.y, box.y + box.height):
-			if box.x + box.width == obst.x:
-				platform.game_over = True
-				box.vel = 0
-				platform.obstacles_vel = 0
+		if type(obst) == Rectangle:
+			if box.acc_down:
+				if box.y + 40 == obst.y:
+					if box.x in range(obst.x, obst.x + obst.width) or obst.x in range(box.x, box.x + box.width):
+						box.acc_down = False
+						platform.obstacle_under_box = obst
+			if box.y in range(obst.y, obst.y + obst.height) or obst.y in range(box.y, box.y + box.height):
+				if obst.x in range(box.x, box.x + box.width+1):
+					platform.game_over = True
+					box.vel = 0
+					platform.obstacles_vel = 0
+		else:
+			if obst.y-20 in range(box.y, box.y + box.height):
+				if obst.x in range(box.x, box.x + box.width+1):
+					platform.game_over = True
+					box.vel = 0
+					platform.obstacles_vel = 0
+			if obst.apex_x in range(box.x, box.x + box.width+1):
+				if box.y + 40 >= obst.apex_y:
+					platform.game_over = True
+					box.vel = 0
+					platform.obstacles_vel = 0
 
 	if platform.obstacle_under_box:
 		if box.x not in range(platform.obstacle_under_box.x, platform.obstacle_under_box.x+platform.obstacle_under_box.width):
@@ -193,5 +235,5 @@ while run:
 			platform.obstacle_under_box = None
 
 	pygame.display.flip()
-	clock.tick(40)
+	# clock.tick(35)
 pygame.quit()
