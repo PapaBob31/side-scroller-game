b471a695c53b9197d4758cf51a92eb82ab6ffc41
damola <egbebitimi121@gmail.com>
2023-03-09 01:40:03 -0800

    Done refactoring for now

--- sidescroller.py
+++ sidescroller.py
@@ -18,28 +18,30 @@ class Player:
 		self.color = red
 		self.width = 40
 		self.height = 40
-		self.vel = 0
-		self.jump_count = 0
+		self.vel = 0 # Speed of the Player when jumping
+		self.jump_count = 0 # keeps track of how far the Player has jumped
 
 	def display(self):
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
-	def move(self):
-		if platform.obstacle_under_box and box.vel > 0:
-			return
+	def jump(self):
+		"""
+			Makes the Player jump. Player can only start jumping when it's on an Obstacle,
+			on the Platform or it's vel attribute is zero 
+		"""
+		self.y += self.vel # Makes Player jump by reducing the y coordinate
 		self.jump_count += 1
-		self.y += self.vel
 
-		if self.jump_count == -4:
-			self.vel = -5
-		if self.jump_count == 0:
+		if self.jump_count == -4: # As Player approaches maximum height
+			self.vel = -5 # reduce jump speed 
+		if self.jump_count == 0: # Maximum height
 			self.jump_count = 1
-			self.vel = 5
-			return
+			self.vel = 5 # Player moves towards the ground/platform
 
 		if self.jump_count == 5:
+			# Increases the speed after a point when moving towards the ground
 			self.vel = 10
-		if self.y == 330: # initial position of player on ground level
+		if self.y == 330: # initial position of player on ground level/platform
 			self.vel = 0
 			self.jump_count = -10
 
@@ -91,7 +93,7 @@ class Platform():
 							   Rectangle(800, 290, 80, 80), Rectangle(800, 250, 80, 40, True), Rectangle(800, 250, 120, 120), 
 							   Rectangle(800, 250, 80, 120), Rectangle(800, 210, 80, 40, True), Rectangle(800, 290, 120, 80)]
 
-		# List to store both Obstacles and Spikes on screen
+		# List to store both Obstacles and Spikes on screen. The Game starts with these preset obstacles and spikes
 		self.obstacles_onscreen = [Triangle(640, 370, 20), Triangle(660, 370, 20),
 								   Triangle(680, 370, 20), Rectangle(700, 330, 80, 40)]
 		self.obstacle_under_box = None
@@ -105,7 +107,7 @@ class Platform():
 
 	def move_and_display_obstacles(self):
 		for obst in self.obstacles_onscreen[:]:
-			obst.display()			
+			obst.display()		
 			if type(obst) == Triangle:
 				obst.x -= platform.vel
 				obst.apex_x -= platform.vel
@@ -121,16 +123,15 @@ class Platform():
 				platform.obstacles_onscreen.remove(obst)
 
 	def check_for_collisions(self, obstacle):
-		""" Checks for collisions between an Obstacle and a Player """
+		""" Checks for collisions between an Obstacle or Spike and a Player """
 		if type(obstacle) == Rectangle:
-			if box.vel > 0:
-				if box.y + box.height == obstacle.y:
-					# if player lands on rectangular obstacle, It should stop and stay on the obstacle
-					if box.x + box.width >= obstacle.x and obstacle.x + obstacle.width >= box.x:
-						platform.obstacle_under_box = obstacle
-						return
+			# if player lands on an obstacle, It should stop and stay on the obstacle
+			if box.y + box.height == obstacle.y:
+				if box.x + box.width >= obstacle.x and obstacle.x + obstacle.width >= box.x:
+					platform.obstacle_under_box = obstacle
+					return
 
-			# if player collides with rectangular obstacle
+			# if player collides with an obstacle
 			if box.y + box.height > obstacle.y and obstacle.y + obstacle.height > box.y:
 				if obstacle.x == box.x + box.width:
 					self.game_over = True
@@ -138,33 +139,32 @@ class Platform():
 		else:	
 			if box.y + 40 >= obstacle.apex_y:
 				if box.vel == 0 and box.x + box.width == obstacle.x: # if player collides with spikes
-					platform.game_over = True
+					self.game_over = True
 				if box.x + box.width >= obstacle.apex_x and obstacle.apex_x > box.x: # if player lands on spikes
 					self.game_over = True
 
 def createSpikes(no_of_spikes, last_obst):
-	""" 
-		A certain amount of distance needs to be between different types of obstacles.
-		Spikes fill that distance. Each spike is 20px wide so the no_of_spikes param is the no
-		of spikes needed to fill up a particular distance 
-	"""
+	""" Creates a number of spikes based on the value of no_of_spikes parameter """
 	spike_x = last_obst.x + last_obst.width
 	for no in range(0, no_of_spikes):
 		spike = Triangle(spike_x, 370, 20)
 		spike = copy.copy(spike)
 		platform.obstacles_onscreen.append(spike)
-		# Position the next spike directly after the last obstacle or spike on screen
-		spike_x = platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width
+		# Positions the next spike directly after the last obstacle or spike on screen
+		spike_x = platform.obstacles_onscreen[-1].x + platform.obstacles_onscreen[-1].width 
 
 def create_next_obstacle():
+	""" 
+		Creates obstacles and spikes to be rendered. Creates the needed spacing between the 
+		current last obstacle and the new one based on some conditions. Spikes fills these spacings
+	"""
 	obstacle = random.choice(platform.obstacles_list)
 	platform.next_obstacle = copy.copy(obstacle)
 
 	# if the next obstacle is lower than the last obstacle on screen with 40px
 	if (obstacle.y - 40) == platform.obstacles_onscreen[-1].y:
 		if obstacle.is_floating:
-			# fills up the empty space under the floating obstacle with a number of 
-			# spikes gotten from obstacle.width//20 since a spike has a base width of 20px.
+			# More spikes need to be created to fill up the space underneath it
 			createSpikes(2 + obstacle.width//20, platform.obstacles_onscreen[-1])
 		else:
 			createSpikes(2, platform.obstacles_onscreen[-1])
@@ -189,9 +189,7 @@ def create_next_obstacle():
 def createObstaclesAndSpikes():
 	create_next_obstacle()
 	if platform.next_obstacle.is_floating:
-		# no of spikes that were just added to obstacles_onscreen, the negative value will be used as an index
-		# to get the first spike from the set of spikes that were just added to platform.obstacles_onscreen
-		index = platform.next_obstacle.width//20
+		index = platform.next_obstacle.width//20 # Number of spikes that were just added to platform.obstacles_onscreen
 		
 		# Setting their x coordinates to the same value will make platform.next_obstacle float 
 		# over all spikes that were just added to obstacles_onscreen when they are rendered.
@@ -202,8 +200,8 @@ def createObstaclesAndSpikes():
 	platform.obstacles_onscreen.append(platform.next_obstacle)
 	platform.next_obstacle = None
 
-# Resets all the important game values to default when called
 def reset():
+	""" Resets all the important game values to default """
 	pygame.mixer.music.play(-1, 0, 0)
 	platform.__init__()
 	box.__init__()
@@ -243,6 +241,7 @@ while run:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
 				if platform.obstacle_under_box or box.vel == 0:
+					# If Player is on top of an obstacle or on the platform itself
 					box.vel = -10
 					box.jump_count = -10
 			if event.key == pygame.K_SPACE:
@@ -255,22 +254,24 @@ while run:
 						pause_game(True)
 
 	win.fill((0, 0, 0))
-	win.blit(bg_img, (i, 0)) # Moving first background image to produce the illusion of moving player
-	win.blit(bg_img, (800+i, 0)) # Moving second background image to produce the illusion of moving player 
+	win.blit(bg_img, (i, 0)) # Background Image 1
+	win.blit(bg_img, (800+i, 0)) # Background Image 2
 	win.blit(text.render("SCORE: " + str(platform.score), True, green), (700, 10))
-
-	box.display()
-	platform.display()
-
+	if platform.game_over:
+		game_over()
 	if platform.game_paused:
 		display_msg("GAME PAUSED!", "Press SPACE to resume")
+	box.display()
+	platform.display()
 
 	if not platform.game_over and not platform.game_paused:
 		if i == -800:
 			i = 0
-		i -= platform.vel
+		i -= platform.vel # Moves background images to produce the illusion of moving player 
 
-		box.move()
+		if not (platform.obstacle_under_box and box.vel > 0):
+		# Prevents Player from moving downwards when already on an obstacle
+			box.jump()
 
 		if platform.obstacles_onscreen[-1].x < 800:
 			createObstaclesAndSpikes()
@@ -280,9 +281,6 @@ while run:
 		# if the player is no longer on an obstacle
 		if platform.obstacle_under_box.x + platform.obstacle_under_box.width < box.x:
 			platform.obstacle_under_box = None
-
-	if platform.game_over:
-		game_over()
 
 	pygame.display.flip()
 pygame.quit()
